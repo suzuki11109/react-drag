ee3f4cadad43072c787953e3020ed67483290b8c
Suzuki Aki <suzuki11109@gmail.com>
2017-03-01 10:03:55 +0700

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,66 @@
 import React from 'react';
 import update from 'react-addons-update';
-import Draggable from 'react-draggable';
+import ReactDOM from 'react-dom';
+// import Draggable from 'react-draggable';
 
-class App extends React.Component {
+class Draggable extends React.Component {
+  // this.props.position = {
+  //   x,
+  //   y
+  // }
+  dragging = false;
+
   constructor() {
     super();
+
     this.state = {
-      rect: { x: 0, y: 0, width: 100, height: 100 },
-      resizer: { x: 100, y: 0 }
+      x: 0,
+      y: 0
     };
   }
-  handleStart = (e, data) => {
-    console.log('Event: ', e.nativeEvent.offsetX);
-    console.log('Data: ', data);
 
+  onMouseDown = (e) => {
+    console.log(e);
+    this.dragging = true;
+    document.addEventListener('mousemove', this.onMouseMove, true);
   }
 
-  resizerDrag = (e, data) => {
-    console.log('Event: ', e);
-    console.log('Data: ', data);
-    this.setState({
-      rect: update(this.state.rect, {width: { $set: data.x }}),
-      resizer: {x: data.x, y: this.state.resizer.y}
-    });
+  onMouseMove = (e) => {
+    if (this.dragging) {
+      let node = ReactDOM.findDOMNode(this);
+
+      // let body = node.props.offsetParent;
+      // console.log(body.scrollLeft);
+      // console.log(e.clientX);
+
+      let offsetParentRect = node.parentElement.getBoundingClientRect();
+      let offsetClientRect = node.getBoundingClientRect();
+      console.log(e.clientX - offsetParentRect.left)
+      this.setState({x:e.clientX - offsetParentRect.left - offsetClientRect.left, y: this.state.y})
+    }
   }
 
+  onMouseUp = () => {
+    this.dragging = false;
+    document.removeEventListener('mousemove', this.onMouseMove, true);
+  }
+
+  render() {
+    return React.cloneElement(React.Children.only(this.props.children), {
+      onMouseDown: this.onMouseDown,
+      onMouseUp: this.onMouseUp,
+      x: this.state.x,
+      y: this.state.y,
+    })
+  }
+}
+
+class App extends React.Component {
   render() {
     return (
       <div style={{marginLeft: 50}}>
         <svg xmlns="http://www.w3.org/2000/svg" width={500} height={500}>
-          <Draggable
-            handle=".figure"
-            onStart={this.handleStart}
-            onDrag={this.handleDrag}
-            onStop={this.handleStop}
-            position={{x: this.state.rect.x, y: this.state.rect.y}}>
-            <rect className="figure" width={this.state.rect.width} height={100} fill={'orange'}/>
-          </Draggable>
-          <g>
-            <Draggable
-              axis="x"
-              handle=".resizer"
-              defaultPosition={this.state.resizer}
-              onDrag={this.resizerDrag}
-              position={{x: this.state.resizer.x, y: this.state.resizer.y}}
-              onStop={this.handleStop}>
-              <rect className="resizer" width={10} height={10} />
-            </Draggable>
-          </g>
+          <Draggable><rect width={100} height={100} /></Draggable>
         </svg>
       </div>
     );
